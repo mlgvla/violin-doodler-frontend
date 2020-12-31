012291e9ee0ff8d5923dbf27c9a9b15ae3054d65
mlgvla <mlgerardvla@gmail.com>
2020-12-31 18:11:53 -0500

    commented out test code and deleted test HTML

--- src/index.js
+++ src/index.js
@@ -40,10 +40,14 @@ function getMelody(e) {
         .then(melody => {
             //must get rid of outer quotes!!
             //JSON require string literals to be in double quotes in order to parse!!!  But in order to
-            //store as a string of nested arrays, I need the single quotes.  The Regex makes it parsable!
-            
-            melodyNotes = JSON.parse(melody.data.attributes.notes.replace(/'/g, '"'))
+            //store as a string of nested arrays, I need the single quotes in the seed file.  The Regex makes it parsable!
+            //Will try with escape characters in the seed file.  Update:  that worked!
+            melodyNotes = JSON.parse(melody.data.attributes.notes)
+  
+            //melodyNotes = JSON.parse(melody.data.attributes.notes.replace(/'/g, '"'))
+
             playMelody(melodyNotes)
+        
         })
 }
 
@@ -58,7 +62,7 @@ function getMelody(e) {
 
 // Violin Sandbox Code
 
-const play = document.querySelector("#run")
+//const play = document.querySelector("#run")
 
 const violin = document.querySelector(".violin")
 
@@ -66,15 +70,16 @@ violin.addEventListener("click", (e) => {
    console.log(e.target.dataset.note)
    playNote(e.target.dataset.note)
  }) // click on a note bubbles up to violin!!!!  Very cool!!!
+    // change to mouseDown and mouseUp listeners with noteOn and noteOff handlers
 
 
 
-play.addEventListener("click", async () => {
+// play.addEventListener("click", async () => {
     
-	await Tone.start()
-    console.log("audio is ready")
-    playMelody()
-})
+// 	await Tone.start()
+//     console.log("audio is ready")
+//     playMelody()
+// })
 
 async function playNote(note){
     await Tone.start()
@@ -84,54 +89,28 @@ async function playNote(note){
 
 
 
-function playTest(){
-    const synth = new Tone.Synth().toDestination();
-    Tone.Transport.bpm.value = 200
-    const now = Tone.now()
-    synth.triggerAttackRelease("C4", "4n", now)
-    synth.triggerAttackRelease("E4", "4n", now + .5)
-    synth.triggerAttackRelease("G4", "2n", now + 1)
-}
+// function playTest(){
+//     const synth = new Tone.Synth().toDestination();
+//     Tone.Transport.bpm.value = 200
+//     const now = Tone.now()
+//     synth.triggerAttackRelease("C4", "4n", now)
+//     synth.triggerAttackRelease("E4", "4n", now + .5)
+//     synth.triggerAttackRelease("G4", "2n", now + 1)
+// }
 // Melody Class Functions
-// playNote - helps playMelody - 
-// setup eventlisteners for all notes
+// playNote - helps playMelody - implement mousedown/mouseup to doodle
 // consider renaming parent divs g-string, d-string, etc.
 
 async function playMelody(melody) {  // will eventually play .this
-    console.log("in playMelody")
-    debugger
-    await Tone.start()
-    console.log("audio is ready")
 
+    await Tone.start()
     const synth = new Tone.Synth().toMaster();
     Tone.Transport.bpm.value = 120;
 
-    const mary = [['E4', '4n.'], ['D4', '8n'], ['C4', '4n'], ['D4', '4n'], ['E4', '4n'], ['E4', '4n'], ['E4', '2n'], ['D4', '4n'], ['D4', '4n'], ['D4', '2n'],    ['E4', '4n'], ['G4', '4n'], ['G4', '4n'], ['rest', '4n'], ['E4', '4n.'],   ['D4', '8n'], ['C4', '4n'], ['D4', '4n'], ['E4', '4n'], ['E4', '4n'], ['E4', '4n'], ['E4', '4n'], ['D4', '4n'], ['D4', '4n'], ['E4', '4n'], ['D4', '4n'], ['C4', '1m']]
-
-
-    const lightlyRow = [['E5', '4n'], ['C#5', '4n'], ['C#5', '2n'], ['D5', '4n'], ['B4', '4n'], ['B4', '2n'], ['A4', '4n'], ['B4', '4n'], ['C#5', '4n'], ['D5', '4n'], ['E5', '4n'], ['E5', '4n'], ['E5', '2n'], ['E5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['D5', '4n'], ['B4', '4n'], ['B4', '4n'], ['B4', '4n'], ['A4', '4n'], ['C#5', '4n'], ['E5', '4n'], ['E5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['C#5', '2n'], ['B4', '4n'], ['B4', '4n'], ['B4', '4n'], ['B4', '4n'], ['B4', '4n'], ['C#5', '4n'], ['D5', '2n'], ['C#5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['D5', '4n'], ['E5', '2n'], ['E5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['D5', '4n'], ['B4', '4n'], ['B4', '4n'], ['B4', '4n'], ['A4', '4n'], ['C#5', '4n'], ['E5', '4n'], ['E5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['C#5', '2n']]
-
-  
-    const twinkle =  [['A4', '4n'], ['A4', '4n'], ['E5', '4n'], ['E5', '4n'], ['F#5', '4n'], ['F#5', '4n'], ['E5', '2n'], ['D5', '4n'], ['D5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['B4', '4n'], ['B4', '4n'], ['A4', '2n'], ['E5', '4n'], ['E5', '4n'], ['D5', '4n'], ['D5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['B4', '2n'], ['E5', '4n'], ['E5', '4n'], ['D5', '4n'], ['D5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['B4', '2n'], ['A4', '4n'], ['A4', '4n'], ['E5', '4n'], ['E5', '4n'], ['F#5', '4n'], ['F#5', '4n'], ['E5', '2n'], ['D5', '4n'], ['D5', '4n'], ['C#5', '4n'], ['C#5', '4n'], ['B4', '4n'], ['B4', '4n'], ['A4', '2n']]
-
-//window.onmousedown = (e) => {
-  // Play the melody!
-
-  let t = Tone.now();
+    let t = Tone.now();
  
-
-//   for (const note of melody) {
-//     console.log(note);
-//     if (note[0] !== "rest") {
-//       //synth.triggerAttackRelease(note[0], note[1], t);
-//       synth.triggerAttackRelease(note[0], Tone.Time(note[1]) - 0.1, t);
-//     }
-//     t += Tone.Time(note[1]);
-//   }
-
     for (const note of melody) {
-        console.log(note);
-        let el = document.querySelector(`[data-note="${note[0]}"]`)
+        //let el = document.querySelector(`[data-note="${note[0]}"]`)
         if (note[0] !== "rest") {
             //synth.triggerAttackRelease(note[0], note[1], t);
            //el.style.filter = "brightness(130%) saturate(110%)"
@@ -141,7 +120,6 @@ async function playMelody(melody) {  // will eventually play .this
           //setTimeout(function (){ el.removeAttribute("style") }, Tone.Time(note[1]).toMilliseconds())//set equal to note length
            
         }
-
     t += Tone.Time(note[1]);
     //setTimeout(function (){ el.removeAttribute("style") }, Tone.Time(note[1]).toMilliseconds())//set equal to 
 
